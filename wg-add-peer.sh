f0d85841a2f490273920a6373664ff184233dfa7
moxart <moxart@moxart.ir>
2020-07-08 17:54:39 +0000

    Generate Profile

--- wg-add-peer.sh
+++ wg-add-peer.sh
@@ -19,32 +19,38 @@ else
 	readonly PROFILE_NAME=$3
 	readonly SERVER_KEY_PUBLIC=$(wg show ${WHICH_INTERFACE} public-key)
 
-	echo ${PROFILE_NAME}
-
 	# Generate New Peer Keys
+	echo "--- Generating Client Config For <${PROFILE_NAME}> ---"
+	
+	mkdir -p profiles/${PROFILE_NAME}
+
 	readonly PEER_KEY_PRIVATE=$(wg genkey)
 	readonly PEER_KEY_PUBLIC=$(echo ${PEER_KEY_PRIVATE} | wg pubkey)
 	readonly PEER_KEY_PRE_SHARED=$(wg genpsk)
 
 	# Create PEER Private/Public Keys
-	echo -e ${PEER_KEY_PRIVATE} >> $3-privatekey.key
-	echo -e ${PEER_KEY_PUBLIC} >> $3-publickey.pub
-	echo -e ${PEER_KEY_PRE_SHARED} >> $3-preshared.psk
+	echo -e ${PEER_KEY_PRIVATE} >> profiles/${PROFILE_NAME}/${PROFILE_NAME}-privatekey.key
+	echo -e ${PEER_KEY_PUBLIC} >> profiles/${PROFILE_NAME}/${PROFILE_NAME}-publickey.pub
+	echo -e ${PEER_KEY_PRE_SHARED} >> profiles/${PROFILE_NAME}/${PROFILE_NAME}-preshared.psk
 
 	# Add New Peer
-	wg set ${WHICH_INTERFACE} peer ${PEER_KEY_PUBLIC} preshared-key ./${PROFILE_NAME}-preshared.psk allowed-ips $1/32
+	wg set ${WHICH_INTERFACE} peer ${PEER_KEY_PUBLIC} preshared-key ./profiles/${PROFILE_NAME}/${PROFILE_NAME}-preshared.psk allowed-ips $1/32
 	wg-quick save ${WHICH_INTERFACE}
 
 	# Generate New Config File
-	echo -e "[Interface]\n" >> $3.conf
-	echo Address = $1/24 >> $3.conf
-	echo PrivateKey = $PEER_KEY_PRIVATE >> $3.conf
-	echo -e "DNS = 1.1.1.1\n" >> $3.conf
-	
-	echo -e "[Peer]\n" >> $3.conf
-	echo PublicKey = ${SERVER_KEY_PUBLIC} >> $3.conf
-	echo PresharedKey = ${PEER_KEY_PRE_SHARED} >> $3.conf
-	echo AllowedIPs = 0.0.0.0/0, ::/0 >> $3.conf
-	echo Endpoint = $2:51820 >> $3.conf
+	cat <<END_OF_CONFIG >> profiles/${PROFILE_NAME}/$3.conf 
+"[Interface]
+Address = $1/24
+PrivateKey = $PEER_KEY_PRIVATE
+DNS = 1.1.1.1
+
+[Peer]
+PublicKey = ${SERVER_KEY_PUBLIC} >> $3.conf
+PresharedKey = ${PEER_KEY_PRE_SHARED}
+AllowedIPs = 0.0.0.0/0, ::/0
+Endpoint = $2:51820
+END_OF_CONFIG
+
 fi
+
 
