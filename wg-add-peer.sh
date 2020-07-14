e44fddd0c1f43ecf9a36f3453534dc7781f2a221
moxart <moxart@moxart.ir>
2020-07-14 17:30:45 +0000

    optimize peer adding

--- wg-add-peer.sh
+++ wg-add-peer.sh
@@ -1,56 +1,59 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]
+# Unset variables
+unset WHICH_INTERFACE
+unset PROFILE_NAME
+unset SERVER_KEY_PUBLIC
+unset ALLOWED_IPS
+unset ENDPOINT
 
-then
+# Initialize Default Variables
+read -rp 'Which Interface ( wg0 ): ' WHICH_INTERFACE
+read -rp 'Profile Name: ' PROFILE_NAME
+read -rp 'Allowed IPs: ' ALLOWED_IPS
+read -rp 'End Point ( Server Public IP ): ' ENDPOINT
 
-	echo "There are no parameters provided to a command!"
+# Check The OS Version
+if [[ -e /etc/debian_version ]]; then
+  source /etc/os-release
+  OS=$ID
+fi
 
-else
+# Install Ubuntu Packages
+if [[ $OS = "ubuntu" ]]; then
+  apt-get install -y software-properties-common -y
+  # shellcheck disable=SC2046
+  apt-get install linux-headers-$(uname --kernel-release) -y
+  add-apt-repository ppa:wireguard/wireguard -y
+  apt-get update -y
+  apt-get install wireguard -y
+  apt-get install qrencode iptables resolvconf -y
+fi
 
-	# Check The OS Version
-	if [[ -e /etc/debian_version ]]; then
-		source /etc/os-release
-		OS=$ID
-	fi
+readonly SERVER_KEY_PUBLIC=$(wg show "${WHICH_INTERFACE:-wg0}" public-key)
 
-	# Install Ubuntu Packages
-	if [[ $OS = "ubuntu" ]]; then
-		apt-get install -y software-properties-common -y
-		apt-get install linux-headers-$(uname --kernel-release) -y
-		add-apt-repository ppa:wireguard/wireguard -y
-		apt-get update -y
-		apt-get install wireguard -y
-		apt-get install qrencode iptables resolvconf -y 
-	fi
+# Generate New Peer Keys
+echo "--- Generating Client Config For <${PROFILE_NAME}> ---"
 
-	# Initialize Default Variables
-	readonly WHICH_INTERFACE="wg0"
-	readonly PROFILE_NAME=$3
-	readonly SERVER_KEY_PUBLIC=$(wg show ${WHICH_INTERFACE} public-key)
+mkdir -p profiles/"${PROFILE_NAME}"
 
-	# Generate New Peer Keys
-	echo "--- Generating Client Config For <${PROFILE_NAME}> ---"
-	
-	mkdir -p profiles/${PROFILE_NAME}
+readonly PEER_KEY_PRIVATE=$(wg genkey)
+readonly PEER_KEY_PUBLIC=$(echo "${PEER_KEY_PRIVATE}" | wg pubkey)
+readonly PEER_KEY_PRE_SHARED=$(wg genpsk)
 
-	readonly PEER_KEY_PRIVATE=$(wg genkey)
-	readonly PEER_KEY_PUBLIC=$(echo ${PEER_KEY_PRIVATE} | wg pubkey)
-	readonly PEER_KEY_PRE_SHARED=$(wg genpsk)
+# Create PEER Private/Public Keys
+echo -e "${PEER_KEY_PRIVATE}" >> profiles/"${PROFILE_NAME}"/"${PROFILE_NAME}"-privatekey.key
+echo -e "${PEER_KEY_PUBLIC}" >> profiles/"${PROFILE_NAME}"/"${PROFILE_NAME}"-publickey.pub
+echo -e "${PEER_KEY_PRE_SHARED}" >> profiles/"${PROFILE_NAME}"/"${PROFILE_NAME}"-preshared.psk
 
-	# Create PEER Private/Public Keys
-	echo -e ${PEER_KEY_PRIVATE} >> profiles/${PROFILE_NAME}/${PROFILE_NAME}-privatekey.key
-	echo -e ${PEER_KEY_PUBLIC} >> profiles/${PROFILE_NAME}/${PROFILE_NAME}-publickey.pub
-	echo -e ${PEER_KEY_PRE_SHARED} >> profiles/${PROFILE_NAME}/${PROFILE_NAME}-preshared.psk
+# Add New Peer
+wg set "${WHICH_INTERFACE:-wg0}" peer "${PEER_KEY_PUBLIC}" preshared-key ./profiles/"${PROFILE_NAME}"/"${PROFILE_NAME}"-preshared.psk allowed-ips "${ALLOWED_IPS}"/32
+wg-quick save "${WHICH_INTERFACE:-wg0}"
 
-	# Add New Peer
-	wg set ${WHICH_INTERFACE} peer ${PEER_KEY_PUBLIC} preshared-key ./profiles/${PROFILE_NAME}/${PROFILE_NAME}-preshared.psk allowed-ips $1/32
-	wg-quick save ${WHICH_INTERFACE}
-
-	# Generate New Config File
-	cat <<END_OF_CONFIG >> profiles/${PROFILE_NAME}/$3.conf 
+# Generate New Config File
+cat <<END_OF_CONFIG >> profiles/"${PROFILE_NAME}"/"${PROFILE_NAME}".conf
 [Interface]
-Address = $1/24
+Address = ${ALLOWED_IPS}/24
 PrivateKey = $PEER_KEY_PRIVATE
 DNS = 1.1.1.1
 
@@ -58,14 +61,13 @@ DNS = 1.1.1.1
 PublicKey = ${SERVER_KEY_PUBLIC}
 PresharedKey = ${PEER_KEY_PRE_SHARED}
 AllowedIPs = 0.0.0.0/0, ::/0
-Endpoint = $2:51820
+Endpoint = ${ENDPOINT}:51820
 END_OF_CONFIG
 
-	# Generate QR-CODE For Profile
-	qrencode -t ansiutf8 < profiles/${PROFILE_NAME}/${PROFILE_NAME}.conf
-	qrencode -o profiles/${PROFILE_NAME}/${PROFILE_NAME}.png < profiles/${PROFILE_NAME}/${PROFILE_NAME}.conf
+# Generate QR-CODE For Profile
+qrencode -t ansiutf8 < profiles/"${PROFILE_NAME}"/"${PROFILE_NAME}".conf
+qrencode -o profiles/"${PROFILE_NAME}"/"${PROFILE_NAME}".png < profiles/"${PROFILE_NAME}"/"${PROFILE_NAME}".conf
 
-	# Compress Config Files & Remove Client Directory Files
-	tar czvf profiles/${PROFILE_NAME}.tar.gz profiles/${PROFILE_NAME}
-	rm -rf profiles/${PROFILE_NAME}
-fi
+# Compress Config Files & Remove Client Directory Files
+tar czvf profiles/"${PROFILE_NAME}".tar.gz profiles/"${PROFILE_NAME}"
+rm -rf profiles/"${PROFILE_NAME}"
